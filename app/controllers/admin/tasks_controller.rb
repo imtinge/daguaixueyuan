ae7ad6bc5e52ffd7183d4ba18602c237f06285bd
Derrick <derrickwang57@gmail.com>
2017-12-13 11:50:27 +0800

    admin can edit destroy task

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -9,7 +9,7 @@ class Admin::TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to admin_task_url(@task)
+      redirect_to admin_task_url(@task), notice: "任务创建成功！"
     else
       render :new
     end
@@ -20,6 +20,24 @@ class Admin::TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to [:admin, @task], notice: "任务修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to admin_tasks_url, notice: '成功删除任务.'
+  end
 
   private
     def task_params
