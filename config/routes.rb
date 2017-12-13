d195360a40331ca77102b1121b26ba48be154088
Derrick <derrickwang57@gmail.com>
2017-12-13 11:08:02 +0800

    admin can create task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  root "admin/tasks#new"
+
+  namespace :admin do
+    resources :tasks
+  end
 end
